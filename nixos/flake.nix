084c23371210921f8ec4a6b4c76aa489b3899a97
Moritz Knorr <mail@moritzknorr.de>
2025-08-03 14:17:21 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,11 +3,9 @@
   description = "A basic NixOS configuration with flakes and home-manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    solaar = { url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
@@ -16,6 +14,7 @@
       specialArgs = { inherit home-manager; };
       modules = [
         ./configuration.nix
+        solaar.nixosModules.default
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
