122aafc6e4db187831b3ec073212a4d93bd7c7c1
Moritz Knorr <mail@moritzknorr.de>
2025-08-03 20:47:47 +0200

    Update home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -30,6 +30,7 @@
   home.file.".gitconfig" = { source = ../config/gitconfig.gitconfig; force = true; };
   home.file.".tmux.conf" = { source = ../config/tmux.tmux.conf; force = true; };
   home.file.".vimrc" = { source = ../config/vimrc.vimrc; force = true; };
+  home.file.".bash_profile" = { source = ../config/bash_profile.bash_profile; force = true; };
 
   # Add desktop environment configs from ../hypr/
   home.file.".config/hypr/hyprland.conf" = { source = ../hypr/hyprland.conf; force = true; };
