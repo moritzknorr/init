408362ea92d42c3b9467fb387387e8701be04fe5
Moritz Knorr <mail@moritzknorr.de>
2025-08-15 07:41:33 +0200

    weather ricing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,7 @@
   console.keyMap = "de";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.defaultLocale = "de_DE.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
